29bf08292927420c1d8509781255ae0d035a4d58
kentahayashi <kenta0810hayashi@gmail.com>
2023-04-18 18:44:30 +0900

    Added double digit calculator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let sum = 0;
 window.onload = function () {
   const input = document.getElementById("input");
+  const input2 = document.getElementById("input2");
 };
 
 function showValue(btn) {
@@ -20,4 +21,33 @@ function showValue(btn) {
 function clearValue() {
   sum = 0;
   input.value = sum;
+  sum2 = 0;
+  input2.value = sum2;
+  total = 0;
+}
+
+let sum2 = 0;
+let total = 0;
+
+function add(btn) {
+  if (sum2 == 0) {
+    sum2 = btn.value;
+  } else {
+    sum2 += btn.value;
+  }
+  input2.value = sum2;
+}
+
+function addValue() {
+  total += parseInt(sum2);
+  calculate();
+  console.log(total);
+}
+
+function calculate() {
+  total += parseInt(sum2);
+
+  sum2 = 0;
+  input2.value = sum2;
+  input2.value = total;
 }
